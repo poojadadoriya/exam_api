be36e2576a7388fcfd3bc1d30eadf9648aa5c72e
pooja.dadoriya@taskcraft.in <pooja.dadoriya@taskcraft.in>
2022-10-18 18:25:39 +0530

    rspec for update

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,48 +3,51 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
   before(:each) do |example|
     @company = Company.create(company_name: "task", director_name: "sonu")
-    @user = User.create(username: "sonu", email: "sonu.rathoer@tasmdkd.in", password: "sonu@123", company_id: "1")
+    @user = User.create(username: "sonu", email: "sonu.rathoer@tasmdkd.in", password: "sonu@123", company_id: @company.id)
   end
-
-  describe "POST Create Booking" do
-    it "crreate user" do
+  describe "POST Create user" do
+    it "create user" do
+      
       post "/api/v1/users",  params: {
                                       "data": {
-                                          "username": "Sonu",
+                                          "username": "sonu",
                                           "email": "sonu.rathor@gmail.com",
                                           "password": "Sonu@123",
-                                          "company_id": @company.id
-
+                                          "company_id": @company.id,
+                                        
+                                        }
                                       }
-                                    } 
-     byebug
-       data = JSON.parse(response.body)
-       expect(data['data']).to eq("sonu")
-      expect(response).to have_http_status(422)
+        data = JSON.parse(response.body)
+        expect(data["data"]["picture"]["record"]["username"]).to eq("sonu")
+      expect(response).to have_http_status(200)
     end
   end
 
-
+ 
   describe "POST/users" do
     it "it should login" do
-      post :'/api/v1/auth/login'
-      expect(response).to have_http_status(401)
+      post '/api/v1/auth/login', params: {"email": "sonu.rathoer@tasmdkd.in", "password": "sonu@123"}
+      expect(response).to have_http_status(200)
     end
   end
-
 
   describe "put/users" do
     it "it will update user" do
-     put :'/api/v1/users/id', :params => {:id => 1} 
-     expect(response).to have_http_status(200)
-   end
+     put "/api/v1/users/#{@user.id}",  params: {
+                                            "last_name": "dadoriya"
+                                            }
+                                     
+      data = JSON.parse(response.body)
+      expect(data["data"]["picture"]["record"]["last_name"]).to eq("dadoriya")                                 
+      expect(response).to have_http_status(200)
+    end
   end
 
 
   describe "delete/users" do
     it "it will delete user" do
-      delete :'/api/v1/users/id', :params => {:id => 1}
-      expect(response).to have_http_status(200)
+      delete "/api/v1/users/#{@user.id}"
+      expect(response).to have_http_status(422)
     end
   end
-end
+  end
